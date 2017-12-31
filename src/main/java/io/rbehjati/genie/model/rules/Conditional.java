0b66e8c9cbf6fd85fbcb7b29e62f73ae54a0c4a1
Razieh Behjati <razieh.behjati@gmail.com>
2017-12-31 08:14:05 +0100

    Conditional

--- src/main/java/io/rbehjati/genie/model/rules/Conditional.java
+++ src/main/java/io/rbehjati/genie/model/rules/Conditional.java
@@ -1,10 +1,39 @@
 package io.rbehjati.genie.model.rules;
 
-public class Conditional {
-	
-	private Term leftHandSide;
-	private Term rightHandSide;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 
-	
+public class Conditional {
+
+				private Term leftHandSide;
+				private Term rightHandSide;
+
+				public Conditional given(Term leftHandSide) {
+								if (this.leftHandSide != null) {
+												throw new IllegalArgumentException("Left hand side is already set");
+								}
+								this.leftHandSide = leftHandSide;
+								return this;
+				}
+
+				public Conditional then(Term rightHandSide) {
+								if (this.rightHandSide != null) {
+												throw new IllegalArgumentException("Right hand side is already set");
+								}
+								this.rightHandSide = rightHandSide;
+								return this;
+				}
+
+				public List<Set<Literal>> toDisjunctiveNormalFormOfNegate() {
+								if (rightHandSide == null) {
+												if (leftHandSide == null) {
+																return Collections.emptyList();
+												}
+												return leftHandSide.negate().toDisjunctiveNormalForm();
+								}
+								// p /\ ~q
+								return Formula.and(leftHandSide, rightHandSide.negate()).toDisjunctiveNormalForm();
+				}
 
 }
