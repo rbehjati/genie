5dfe2b6a1860987f8eb56f73b785eb869cb2020b
Razieh Behjati <razieh.behjati@gmail.com>
2017-12-31 08:19:26 +0100

    reorganization in test

--- src/test/java/io/rbehjati/genie/JennyTest.java
+++ src/test/java/io/rbehjati/genie/JennyTest.java
@@ -1,13 +1,7 @@
 package io.rbehjati.genie;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
-
-import io.rbehjati.genie.model.Combination;
-import io.rbehjati.genie.model.Feature;
 
 public class JennyTest {
 
@@ -23,25 +17,6 @@ public class JennyTest {
 								String[] params = {"-n1", "2", "3", "4", "3"};
 								String[] result = new Jenny().generator(params);
 								Assertions.assertThat(result).hasSize(4);
-				}
-
-				@Test
-				public void testFeatureCombinationGenerator() {
-								Feature nameFeature = new Feature("name", "Valid", "Invalid");
-								Feature ageFeature = new Feature("age", "Old", "Young");
-								List<Combination> result = new CombinationGenerator().generateCombinations(2, Arrays.asList(nameFeature, ageFeature));
-
-								Assertions.assertThat(result).hasSize(4);
-
-								for (int i = 0; i < 2; i++) {
-												for (int j = 0; j < 2; j++) {
-																Combination combo = new Combination();
-																combo.addEquivalenceClassForFeature(nameFeature, nameFeature.getClassLabel(i));
-																combo.addEquivalenceClassForFeature(ageFeature, ageFeature.getClassLabel(j));
-																Assertions.assertThat(result.contains(combo));
-												}
-								}
-
 				}
 
 }
