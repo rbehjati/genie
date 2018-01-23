4cb396394fa10d6284e18516255a2d2d044dedc2
Razieh Behjati <razieh.behjati@gmail.com>
2018-01-24 00:40:08 +0100

    Using NativeUtils to load native library from jar

--- src/main/java/io/rbehjati/genie/Jenny.java
+++ src/main/java/io/rbehjati/genie/Jenny.java
@@ -1,10 +1,26 @@
 package io.rbehjati.genie;
 
+import cz.adamh.utils.NativeUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class Jenny {
 
-	static {
-		System.loadLibrary("jenny");
-	}
+    private static final Logger logger = LoggerFactory.getLogger(Jenny.class);
 
-	public native String[] generator(String[] argv);
+    static {
+        try {
+            System.loadLibrary("jenny");
+        } catch (UnsatisfiedLinkError e) {
+            try {
+                NativeUtils.loadLibraryFromJar("/" + System.mapLibraryName("jenny"));
+            } catch (IOException e1) {
+                logger.error(e1.getMessage(), e1);
+            }
+        }
+    }
+
+    public native String[] generator(String[] argv);
 }
